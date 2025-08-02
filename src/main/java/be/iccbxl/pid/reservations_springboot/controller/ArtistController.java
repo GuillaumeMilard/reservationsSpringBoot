59711bbbe7dd629ebf0bb91d90afa0d0894dddc5
Guillaume Ndoumbe <milard_cmr@yahoo.fr>
2025-08-02 11:36:58 +0200

    Form update Artist

--- src/main/java/be/iccbxl/pid/reservations_springboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/controller/ArtistController.java
@@ -2,11 +2,16 @@ package be.iccbxl.pid.reservations_springboot.controller;
 
 import be.iccbxl.pid.reservations_springboot.model.Artist;
 import be.iccbxl.pid.reservations_springboot.service.ArtistService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
@@ -31,5 +36,42 @@ public class ArtistController {
         model.addAttribute("title", "Fiche d'un artiste");
         return "artist/show";
     }
+
+    @GetMapping("/artists/{id}/edit")
+    public String edit(Model model, @PathVariable long id, HttpServletRequest request) {
+
+        Artist artist = service.getArtist(id);
+        model.addAttribute("artist", artist);
+
+        //Générer le lien retour pour l'annulation
+        String referrer = request.getHeader("Referer");
+
+        if(referrer!=null && !referrer.equals("")) {
+            model.addAttribute("back", referrer);
+        } else {
+            model.addAttribute("back", "/artists/"+artist.getId());
+        }
+        return "artist/edit";
+    }
+
+
+    @PutMapping("/artists/{id}/edit")
+    public String update(@Valid @ModelAttribute Artist artist, BindingResult bindingResult, @PathVariable long id, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "artist/edit";
+        }
+
+        Artist existing = service.getArtist(id);
+
+        if(existing==null) {
+            return "artist/index";
+        }
+
+        service.updateArtist(id, artist);
+
+        return "redirect:/artists/"+artist.getId();
+    }
+
 
 }
