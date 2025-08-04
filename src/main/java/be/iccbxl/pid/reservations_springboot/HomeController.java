0d0f69c752a35f631e6eed1320e60258b2ae424b
Guillaume Ndoumbe <milard_cmr@yahoo.fr>
2025-08-04 06:22:29 +0200

    Frontend authentication - Login

--- src/main/java/be/iccbxl/pid/reservations_springboot/HomeController.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/HomeController.java
@@ -1,14 +1,14 @@
 package be.iccbxl.pid.reservations_springboot;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot !!!";
+        return "home";
     }
-
 }
