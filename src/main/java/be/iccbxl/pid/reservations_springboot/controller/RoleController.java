67fa14e479441954230678929102423a3b9840a9
Guillaume Ndoumbe <milard_cmr@yahoo.fr>
2025-08-12 02:45:11 +0200

    Validation + Sécurisation formulaires entité Role - close #45 #46 #26

--- src/main/java/be/iccbxl/pid/reservations_springboot/controller/RoleController.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/controller/RoleController.java
@@ -8,9 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -120,7 +118,7 @@ public class RoleController {
     /**
      * UPDATE - mise à jour d’un rôle dont l'id est passé dans l'URL
      */
-    @PostMapping("/roles/{id}")
+    @PutMapping("/roles/{id}")
     public String update(@PathVariable Long id,
                          @Valid Role role,
                          BindingResult bindingResult,
@@ -150,7 +148,7 @@ public class RoleController {
     /**
      * DELETE - suppression d’un rôle dont l'id est passé dans l'URL
      */
-    @PostMapping("/roles/{id}/delete")
+    @DeleteMapping("/roles/{id}/delete")
     public String delete(@PathVariable Long id,
                          RedirectAttributes redirAttrs) {
         // Récupérer le rôle dont l'id est passé en paramètre dans l'URL
--- src/main/java/be/iccbxl/pid/reservations_springboot/model/Role.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/model/Role.java
@@ -1,6 +1,8 @@
 package be.iccbxl.pid.reservations_springboot.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +17,11 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Using UserRole enum to define the role names
     @Enumerated(EnumType.STRING)
+    @NotBlank(message = "Role name cannot be blank")
+    @Size(min = 2, max = 60, message = "Role name must be between 2 and 60 characters")
+    @Column(name = "name", nullable = false, unique = true)
     private UserRole name;
 
     @Override
